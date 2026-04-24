d7b3c198a21063a877a5a0521e7cdf018526edd9
Soroush Hosseini <swrshho@gmail.com>
2026-04-24 19:08:14 +0200

    first attempt at telegram bot

--- web/app/api/telegram/route.ts
+++ web/app/api/telegram/route.ts
@@ -1,9 +1,36 @@
+import { writeClient } from "@/lib/sanity/clients";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-    const body = await req.json();
+    try {
+        const body = await req.json();
 
-    console.log("Telegram update:", body);
+        const text = body.message?.text;
 
-    return Response.json({ ok: true });
+        if (!text) {
+            return Response.json({ ok: true });
+        }
+        console.log("STEP 1: creating post in Sanity");
+
+        const result = await writeClient.create({
+            _type: "post",
+            title: text.substring(0, 25),
+            body: text,
+            slug: {
+                _type: "slug",
+                current: text.toLowerCase().replace(/\s+/g, "-"),
+            },
+            content: [
+                {
+                    _type: "block",
+                    children: [{ _type: "span", text }],
+                },
+            ],
+        });
+
+        console.log("✅ Sanity success:", result);
+        return Response.json({ ok: true });
+    } catch (err) {
+        console.error("❌ Sanity error:", err);
+    }
 }
--- web/lib/sanity/clients.ts
+++ web/lib/sanity/clients.ts
@@ -7,3 +7,12 @@ export const client = createClient({
     apiVersion: "2024-01-01",
     useCdn: true,
 })
+
+
+export const writeClient = createClient({
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+    apiVersion: "2024-01-01",
+    token: process.env.SANITY_API_TOKEN, // IMPORTANT
+    useCdn: false,
+});
